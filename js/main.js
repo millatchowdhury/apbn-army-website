d9eb2a0f2eed5e9d68c2289ac145a4eea6044fde
millatchowdhury <millatchowdhury@gmail.com>
2024-08-04 00:47:26 +0600

    ok

--- js/main.js
+++ js/main.js
@@ -6,7 +6,23 @@ $(document).ready(function() {
         }
         // other options
       });
-    
+    // apn units 
+    $('.text.owl-carousell').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
 
    
   });
